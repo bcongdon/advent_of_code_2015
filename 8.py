85b21cdee8e38e4fc1ca76db3234dfc6d6b77e78
Benjamin Congdon <bencongdon96@gmail.com>
2015-12-08 16:34:12 -0600

    Day 8

---
+++ 8.py
@@ -0,0 +1,16 @@
+strings = list
+
+with open('8.txt','r') as f:
+	strings = f.read().split()
+
+def num_string_total(string):
+	return len(eval(string))
+
+def num_literals(string):
+	return len(string) 
+
+def len_encode(string):
+	return 2 + string.count('"') + string.count("\\")
+
+print sum([num_literals(i) for i in strings]) - sum([num_string_total(i) for i in strings])
+print sum([len_encode(i) for i in strings])
